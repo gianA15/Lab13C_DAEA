cd15f178ee605e94cbe149046b39590bf14a257d
gianA15 <gian.arce@tecsup.edu.pe>
2023-11-30 21:55:34 -0500

    Agregar archivos de proyecto.

---
+++ Lab13C/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab13C.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
